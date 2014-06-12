52235dc5980728475d6e9ad2638fc0f0e5c4de3b
jgerardsimcock <justin_simcock@yahoo.com>
2014-06-11 21:14:02 -1000

    First Fail

--- spec/mathfunctions_spec.js
+++ spec/mathfunctions_spec.js
@@ -1,9 +1,10 @@
 describe("add", function() {
   var result;
-  });
+ 
 
   it("should add two operators", function() {
-    reult = add(2,3);
-    expect(add).toBe(5);
+    result = add(2,3);
+    expect(result).toBe(5);
+ });
 
-  });
+});
